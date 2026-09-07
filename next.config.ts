a010e7e9138b4e99f5b68bcc7b6eda594939747c
Sortelba <steffen.or@googlemail.com>
2026-09-07 23:43:05 +0200

    Manuelles Update: 2026-09-07 23:43:05

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
+const projectRoot = process.cwd();
+
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: projectRoot,
+  },
   output: process.env.GITHUB_ACTIONS === "true" ? "export" : undefined,
   trailingSlash: process.env.GITHUB_ACTIONS === "true" ? true : undefined,
   images: {
